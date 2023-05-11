fab14e368641815f58b210eb94cadc707115b23b
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2023-05-11 21:35:40 +0200

    modifications du layout responsive

--- src/pages/CastleDetails.jsx
+++ src/pages/CastleDetails.jsx
@@ -20,45 +20,59 @@ function CastleDetails({ castle }) {
   lat = castle.data.point.lat;
 
   return (
-    <div className="h-[80%] w-[80%] max-w-[1280px]">
-      <h2 className="text-center font-montserrat text-[1.6rem] font-bold mt-[1rem]">
+    <div className="max-lg:w-[95%] h-[80%] w-[80%] max-w-[1280px]">
+      <h2 className="max-md:pt-12 text-center font-montserrat text-[1.6rem] font-bold mt-[1rem]">
         {castle.data.name}
       </h2>
       <div className="p-[3rem]">
-        <div className="grid grid-cols-2 grid-row-3">
-          <div className="">
+        <div className="grid max-md:grid-cols-1 grid-cols-2">
+          <div className="max-md:row-start-1 max-md:row-end-2">
             <img
               className="max-h-[300px] max-w-[90%] border-[2px] border-primary drop-shadow-[-4px_4px_4px_rgba(0,0,0,0.25)]"
               src={src}
               alt={castle.data.name}
             />
           </div>
-          <div className="row-start-2 row-end-3">
+          <div className="max-md:row-start-2 max-md:row-end-3 row-start-2 row-end-3">
             <ul className="pt-[0.8rem]">
-              <li>Pays : {castle.data.address.country}</li>
-              <li>Région : {castle.data.address.state}</li>
-              <li>Ville : {castle.data.address.village}</li>
-              <li>Code Postal : {castle.data.address.postcode}</li>
+              <li>
+                <span className="font-semibold">Pays :</span>{" "}
+                {castle.data.address.country}
+              </li>
+              <li>
+                <span className="font-semibold">Région :</span>{" "}
+                {castle.data.address.state}
+              </li>
+              <li>
+                <span className="font-semibold">Ville :</span>{" "}
+                {castle.data.address.village}
+              </li>
+              <li>
+                <span className="font-semibold">Code Postal :</span>{" "}
+                {castle.data.address.postcode}
+              </li>
             </ul>
           </div>
-          <div className="row-start-3 row-end-4 flex flex-col justify-end">
-            <h2 className="pt-[1.2rem] font-bold">
+          <div className="max-md:row-start-4 max-md:row-end-5 row-start-3 row-end-4">
+            <h2 className="pt-[1.2rem] font-bold text-center mb-4">
               En avant pour l'aventure !
             </h2>
-            <ul>
-              <li className="hover:text-secondary">
+            <div className="flex flex-row justify-between px-8">
+              <button className="p-2 rounded basicButton cursor-pointer flex">
                 <a href={castle.data.url}>Booking</a>
-              </li>
-              <li className="hover:text-secondary">
+              </button>
+              <button className="p-2 rounded basicButton cursor-pointer flex">
                 <a href={castle.data.wikipedia}>Wikipedia</a>
-              </li>
-            </ul>
+              </button>
+            </div>
           </div>
           <div>
-            <h2 className="font-bold">Description</h2>
+            <h2 className="max-md:pt-[1.2rem] max-md:row-start-3 max-md:row-end-4 font-bold">
+              Description
+            </h2>
             <p>{text}</p>
           </div>
-          <div className="row-start-2 row-end-4">
+          <div className="pt-[1.2rem] max-md:row-start-5 max-md:row-end-6 row-start-2 row-end-4">
             <GoogleMaps lon={lon} lat={lat} />
           </div>
         </div>
