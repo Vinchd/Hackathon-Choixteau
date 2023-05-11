018e51a58e306cc045f3ea292311c450dd83b710
Samuel Faber <samuel.faber@live.fr>
2023-05-11 16:27:20 +0200

    Import Google Maps Api, composant GoogleMaps

--- src/pages/CastleDetails.jsx
+++ src/pages/CastleDetails.jsx
@@ -1,43 +1,47 @@
 import { useState, useEffect } from "react";
+import GoogleMaps from "../components/GoogleMaps";
 import axios from "axios";
 
 function CastleDetails() {
+  const [castles, setCastles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [castles, setCastles] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    
-const API = "https://api.opentripmap.com/0.1/en/places/xid/H636300?apikey=5ae2e3f221c38a28845f05b6f85b1a4179864a9e8c9917017478344e"
+  const API =
+    "https://api.opentripmap.com/0.1/en/places/xid/H636300?apikey=5ae2e3f221c38a28845f05b6f85b1a4179864a9e8c9917017478344e";
 
-useEffect(() => {
-    axios.get(API)
-    .then((res) => {setCastles(res.data);
-    setIsLoading(false)})
-    .catch((err) => console.error(err))
-}, [])
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((res) => {
+        setCastles(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-if(isLoading) {
-    return (
-        <div>
-            Chargement
-        </div>
-    )
-}
+  if (isLoading) {
+    return <div>Chargement</div>;
+  }
 
-    return(
-        <div>
-            <h2>{castles.name}</h2>
-            <img src={castles.preview.source} alt={castles.name} />
-            <h2>Decription</h2> 
-            <p>{castles.wikipedia_extracts.text}</p>
-            <h2>Carte interactive</h2>
-            
-            <h2>Liens utiles</h2>
-            <ul>
-                <li><a href ={castles.url}>Booking</a></li>
-                <li><a href ={castles.wikipedia}>Wikipedia</a></li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>{castles.name}</h2>
+      <img src={castles.preview.source} alt={castles.name} />
+      <h2>Decription</h2>
+      <p>{castles.wikipedia_extracts.text}</p>
+      <h2>Carte interactive</h2>
+      <GoogleMaps />
+      <h2>Liens utiles</h2>
+      <ul>
+        <li>
+          <a href={castles.url}>Booking</a>
+        </li>
+        <li>
+          <a href={castles.wikipedia}>Wikipedia</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default CastleDetails;
