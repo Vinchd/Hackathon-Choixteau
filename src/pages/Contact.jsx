c2dae5a0b4afa9eed7dac5ac4e23bc739992c742
Samuel Faber <samuel.faber@live.fr>
2023-05-11 23:48:36 +0200

    Gestion du formulaire de contact qui fonctionne

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,32 @@
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
+
 export default function Contact() {
-  const handleSubmit = (event) => event.preventDefault();
+  const form = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_y58p5i8",
+        "template_wxy56xa",
+        form.current,
+        "WwoXTwb7UjUWP496V"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
     <div className="w-full h-[600px] by-28 bg-no-repeat bg-center bg-[url('./assets/fond-contact.png')] flex justify-center">
       <form
+        ref={form}
         onSubmit={handleSubmit}
         className="text-primary flex flex-col items-center justify-center w-[300px]"
       >
@@ -10,13 +34,13 @@ export default function Contact() {
           <label className="font-bold mb-1" htmlFor="name">
             Name
           </label>
-          <input id="name" type="name" />
+          <input id="name" name="user_name" type="name" required />
         </div>
         <div className="flex flex-col mt-4 w-full">
           <label className="font-bold mb-1" htmlFor="email">
             Email
           </label>
-          <input id="email" type="email" />
+          <input id="email" name="user_email" type="email" required />
         </div>
         <div className="flex flex-col mt-4 w-full">
           <label className="font-bold mb-1" htmlFor="message">
@@ -26,11 +50,13 @@ export default function Contact() {
             className="h-40 w-full"
             id="message"
             type="message"
+            name="message"
             placeholder="Saisissez votre message ici"
+            required
           />
         </div>
         <div className="flex justify-center mt-8 contactButton basicButton w-24">
-          <button className="font-bold" type="submit">
+          <button className="font-bold" type="submit" value="Send">
             Submit
           </button>
         </div>
